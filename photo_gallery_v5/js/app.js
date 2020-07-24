af802c81184246c6e16aba6f1fca9d9bbe0a518a
ZiyuZhang-zz <zzhang3@cca.edu>
2020-07-24 10:14:53 -0700

    Update app.js

--- photo_gallery_v5/js/app.js
+++ photo_gallery_v5/js/app.js
@@ -1,9 +1,2 @@
-/* baguetteBox.run('.gallery'); */
-let photoDes = '';
-for (let i=1;i<=12;i++){
-    let x =(i).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false});
-    photoDes += `<a href="photos/${x}.jpg" data-caption="Image caption">
-    <img src="photos/thumbnails/${x}.jpg" alt="First image">
-    </a>`;
-    document.getElementsByClassName('gallery').innerHTML = photoDes;
-}
+baguetteBox.run('.gallery'); 
+const search = new Filter('search', 'data-caption');
